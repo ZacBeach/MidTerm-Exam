68bce4298dd3fbb47784a66aa0f8a44b0aba4345
ZacBeach <02zachary.beach@gulllakecs.org>
2022-03-11 09:52:49 -0500

    Story print statements

---
+++ MidTerm Exam.py
@@ -0,0 +1,4 @@
+print("Once Upon a time I went to Woren Dunes at Lake Michigan.")
+print("Me my dad and some friends went over the dunes and down in to the clay pits.")
+print("It was about a half hour long walk through the stream full of clay water rocks and sticks.")
+print("It was a fun day and a great adventure.")
